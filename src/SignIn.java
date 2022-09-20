1a1f711ed76fab6e81f7b5f562c9fdecb89fedda
SruthySL <113691024+SruthySL@users.noreply.github.com>
2022-09-20 16:50:54 +0530

    Update SignIn.java

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,22 +1,69 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SignIn {
 	
-	@Test
-	public void signIn() {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://mobileworld.banyanpro.com/");
-		LandingPage ob = new LandingPage(driver);
-	    ob.SignIn("Sruthy@gmail.com","Sruthy123");
-	    ob.SignIn("sr", "Sruthy123");
-	    ob.SignIn("Sruthy@gmail.com", "Sr");
-	    ob.SignIn("sruthy#sl@gmail.com", "sruthy123456");
+	 @Test(description="user can signin with valid username and password")
+	    public void  signIn() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn("Nivethaa","Nivi15");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/index.html");
+	    }
 	    
-
-	}
+	    @Test(description="user cant signin by leaving username field empty")
+	    public void  Signinvalid() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn(" ","nivi15");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/sign.html");
+	        }
+	    
+	    @Test(description="user cant signin by leaving both username and password field empty" )
+	    public void  Signinemptyvalid() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn(" "," ");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/sign.html");
+	        }
+	    
+	    @Test(description="user cant signin by entering less than 3 characters in username")
+	    public void  Signinpominavalid() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn("Ni","Nivi153 ");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/sign.html");
+	        }
+	    
+	    @Test(description="user cant signin by entering more than 20 characters in username")
+	    public void  Signinusermoretwentyinavalid() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn("NivethaaElumalaishaaheidii","nivi123 ");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/sign.html");
+	        }
+	    
+	    
+	    @Test(description="user cant signin by entering special characters in password")
+	    public void  Signinpassspecialinavalid() {
+	        System.setProperty("webdriver.chrome.driver","C:/Users/sruthy.sl/Downloads/chromedriver_win32/chromedriver.exe");
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://mobileworld.banyanpro.com/");
+	        LandingPage ob= new LandingPage(driver);
+	        ob.SignIn("nivethaa","nivi!@");
+	        Assert.assertEquals(driver.getCurrentUrl(),"https://mobileworld.banyanpro.com/sign.html");
+	    }
 
 }
